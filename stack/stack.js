b1f81bd3f8d75711bc6ede6b9e9b6e0207f6325c
Abinash Panda <abinashp@protonmail.ch>
2020-08-03 14:36:04 +0530

    stack ds is added with md

--- stack/stack.js
+++ stack/stack.js
@@ -1,3 +1,37 @@
 class Stack {
+    constructor() {
+        this.stack = []
+        this.length = 0
+    }
 
+    // add item to top of stack
+    push(v) {
+        this.stack[this.length] = v
+        this.length++
+        return this.length
+    }
+
+    pop() {
+        if (this.count === 0) return undefined
+        this.length--
+        const removedItem = this.stack[this.length]
+        this.stack = [...this.stack.splice(this.length, 1)]
+        return removedItem
+    }
+
+    peek() {
+        return this.stack[this.length - 1]
+    }
 }
+
+// Example
+let bookShelf = new Stack()
+
+bookShelf.push("The Art of War")
+bookShelf.push("Jungle Book")
+
+bookShelf.peek()
+
+bookShelf.pop()
+
+console.log(bookShelf)
---
+++ stack/stack.md
@@ -0,0 +1,11 @@
+### stack
+
+- stack is a linier data structure
+
+- Last In First Out (LIFO)
+
+- push method add an element to stack and returns length of stack
+
+- pop method removed last added element and returns the removed element
+
+- peek method returns top element
